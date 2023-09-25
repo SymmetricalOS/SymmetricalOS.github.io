e6d0ba7209a254e26e027e633f29ac89e89ceed8
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-09-25 18:16:46 -0400

    Update rss.js

--- rss.js
+++ rss.js
@@ -11,15 +11,15 @@ async function rssToJson(feedLink) {
 var rss = rssToJson("https://symmetricalos.github.io/feeds/rss.xml");
 
 var buffer = "";
-buffer = buffer + wrapHtml(rss["title"], "h1");
-const items = rss["items"];
+buffer = buffer + wrapHtml(rss.title, "h1");
+const items = rss.items;
 
 console.log(typeof(items));
 
 for (const item of items) {
-    var title = wrapHtml(item["title"], "h2");
-    var content = wrapHtml(item["content_html"], "p");
-    var sub = wrapHtml("Published by " + item["author"]["name"], "small");
+    var title = wrapHtml(item.title, "h2");
+    var content = wrapHtml(item.content_html, "p");
+    var sub = wrapHtml("Published by " + item.author.name, "small");
     buffer = buffer + wrapHtml(title + content + sub, "div");
 }
 document.write(buffer);
