896aa36dd4172ef4523f43ad019fcd3c9bc4dd52
Sid <saytleader@outlook.com>
2023-09-27 09:20:42 -0400

    Update rss.js

--- rss.js
+++ rss.js
@@ -2,34 +2,31 @@ function wrapHtml(input, wrapper, extra="") {
     return "<" + wrapper + extra + ">" + input + "</" + wrapper + ">";
 }
 
-async function rssToJson(feedLink) {
+
 	var d;
 
-	var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent(feedLink);
+	var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://symmetricalos.github.io/feeds/rss.xml");
 
 	$.getJSON(reqURL,
-							function (data) {
+							function (rss) {
 								
-								d = data;
+								var buffer = "";
+								buffer = buffer + wrapHtml(rss.title, "h1");
+								const items = rss.items;
+
+								console.log(typeof(items));
+
+								for (const item of items) {
+    								var title = wrapHtml(item.title, "h2");
+    								var content = wrapHtml(item.content_html, "p");
+    								var sub = wrapHtml("Published by " + item.author.name, "small");
+    								buffer = buffer + wrapHtml(title + content + sub, "div");
+								}
+								document.write(buffer);
 							}
 						);
 
 	return d;
-}
 
-var rss = rssToJson("https://symmetricalos.github.io/feeds/rss.xml");
-console.log(rss);
 
-var buffer = "";
-buffer = buffer + wrapHtml(rss.title, "h1");
-const items = rss.items;
 
-console.log(typeof(items));
-
-for (const item of items) {
-    var title = wrapHtml(item.title, "h2");
-    var content = wrapHtml(item.content_html, "p");
-    var sub = wrapHtml("Published by " + item.author.name, "small");
-    buffer = buffer + wrapHtml(title + content + sub, "div");
-}
-document.write(buffer);
