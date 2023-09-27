261a57097786dea49df2e4f510754b62946ba916
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-09-27 16:06:16 -0400

    link

--- rss.js
+++ rss.js
@@ -8,13 +8,13 @@ var reqURL =
 
 $.getJSON(reqURL, function (rss) {
 	var buffer = "";
-	buffer = buffer + wrapHtml(rss.feed.title, "h1");
+	buffer = buffer + wrapHtml(rss.feed.title, "h1")
 	const items = rss.items;
 
 	console.log(rss);
 
 	for (const item of items) {
-		var title = wrapHtml(item.title, "h2");
+		var title = wrapHtml(wrapHtml(item.title, "h2"), "a", " href=\"" + item.link + "\"");
 		var content = wrapHtml(item.content, "p");
 		var sub = wrapHtml(
 			"Published by " + item.author + " on " + item.pubDate,
