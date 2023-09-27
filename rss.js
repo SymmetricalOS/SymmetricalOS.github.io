5372090e8e62cd82484377f76598a039ada036ba
SidGames5 <saytleader@outlook.com>
2023-09-27 15:56:25 -0400

    fix an issue

--- rss.js
+++ rss.js
@@ -1,32 +1,23 @@
-function wrapHtml(input, wrapper, extra="") {
-    return "<" + wrapper + extra + ">" + input + "</" + wrapper + ">";
+function wrapHtml(input, wrapper, extra = "") {
+	return "<" + wrapper + extra + ">" + input + "</" + wrapper + ">";
 }
 
+var reqURL =
+	"https://api.rss2json.com/v1/api.json?rss_url=" +
+	encodeURIComponent("https://symmetricalos.github.io/feeds/rss.xml");
 
-	var d;
+$.getJSON(reqURL, function (rss) {
+	var buffer = "";
+	buffer = buffer + wrapHtml(rss.title, "h1");
+	const items = rss.items;
 
-	var reqURL = "https://api.rss2json.com/v1/api.json?rss_url=" + encodeURIComponent("https://symmetricalos.github.io/feeds/rss.xml");
+	console.log(rss);
 
-	$.getJSON(reqURL,
-							function (rss) {
-								
-								var buffer = "";
-								buffer = buffer + wrapHtml(rss.title, "h1");
-								const items = rss.items;
-
-								console.log(typeof(items));
-
-								for (const item of items) {
-    								var title = wrapHtml(item.title, "h2");
-    								var content = wrapHtml(item.content_html, "p");
-    								var sub = wrapHtml("Published by " + item.author.name, "small");
-    								buffer = buffer + wrapHtml(title + content + sub, "div");
-								}
-								document.getElementByID("news2").innerHTML = buffer;
-							}
-						);
-
-	return d;
-
-
-
+	for (const item of items) {
+		var title = wrapHtml(item.title, "h2");
+		var content = wrapHtml(item.content, "p");
+		var sub = wrapHtml("Published by " + item.author, "small");
+		buffer = buffer + wrapHtml(title + content + sub, "div");
+	}
+	document.getElementById("news2").innerHTML = buffer;
+});
