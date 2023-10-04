05347185b59300511092b66a91ceada93443d8f7
SidGames5 <saytleader@outlook.com>
2023-10-04 19:58:21 -0400

    make the date and time better

--- rss.js
+++ rss.js
@@ -33,8 +33,15 @@ $.ajax({
 				var title = wrapHtml(wrapHtml(item.title, "h2"), "a", " href=\"" + item.link + "\"");
 			}
 			var content = wrapHtml(item.content, "p");
+			var time = item.pubDate;
+			var options = {
+				year: 'numeric', month: 'numeric', day: 'numeric',
+				hour: 'numeric', minute: 'numeric', second: 'numeric'
+			};
+			var formatter = new Intl.DateTimeFormat([], options);
+			var localtime = formatter.format(new Date(new Date(time) - (new Date().getTimezoneOffset() * 1000 * 60)));
 			var sub = wrapHtml(
-				"Published by " + item.author + " on " + item.pubDate + " UTC",
+				"Published by " + item.author + " on " + localtime + " local time",
 				"small"
 			);
 			buffer = buffer + wrapHtml(title + content + sub, "div");
