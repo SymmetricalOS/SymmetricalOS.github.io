5ae7da7161a66ec8a57f6c65f95fa251ad43e96e
SidGames5 <saytleader@outlook.com>
2023-10-04 16:19:20 -0400

    fix rss

--- rss.js
+++ rss.js
@@ -6,25 +6,38 @@ var reqURL =
 	"https://www.toptal.com/developers/feed2json/convert?url=" +
 	encodeURIComponent("https://symmetricalos.github.io/feeds/rss.xml");
 
-$.getJSON(reqURL, function (rss) {
-	var buffer = "";
-	//buffer = buffer + wrapHtml(rss.title, "h1")
-	const items = rss.items;
+$.ajax({
+	type: 'GET',
+	url: reqURL,
+	//jsonpCallback: 'jsonCallback',
+	contentType: 'application/json',
+	//async: false,
+	//dataType: 'jsonp',
 
-	console.log(rss);
+	//xhrFields: { 
+	// for CORS?
+	//  withCredentials: false
+	//},
+	success: function (rss) {
+		var buffer = "";
+		//buffer = buffer + wrapHtml(rss.title, "h1")
+		const items = rss.items;
 
-	for (const item of items) {
-		if (item.link == "") {
-			var title = wrapHtml(item.title, "h2", " title=\"" + item.guid + "\"");
-		} else {
-			var title = wrapHtml(wrapHtml(item.title, "h2"), "a", " href=\"" + item.link + "\"");
+		console.log(rss);
+
+		for (const item of items) {
+			if (item.link == "") {
+				var title = wrapHtml(item.title, "h2", " title=\"" + item.guid + "\"");
+			} else {
+				var title = wrapHtml(wrapHtml(item.title, "h2"), "a", " href=\"" + item.link + "\"");
+			}
+			var content = wrapHtml(item.content_html, "p");
+			var sub = wrapHtml(
+				"Published by " + item.author.name + " on " + item.date_published,
+				"small"
+			);
+			buffer = buffer + wrapHtml(title + content + sub, "div");
 		}
-		var content = wrapHtml(item.content_html, "p");
-		var sub = wrapHtml(
-			"Published by " + item.author.name + " on " + item.date_published,
-			"small"
-		);
-		buffer = buffer + wrapHtml(title + content + sub, "div");
+		document.getElementById("news2").innerHTML = buffer;
 	}
-	document.getElementById("news2").innerHTML = buffer;
-});
+})
