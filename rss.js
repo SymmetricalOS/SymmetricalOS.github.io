7eb7674ecb4e04727a7f94690a90b30e71cac220
SidGames5 <saytleader@outlook.com>
2023-10-04 16:14:01 -0400

    fix rss

--- rss.js
+++ rss.js
@@ -3,12 +3,12 @@ function wrapHtml(input, wrapper, extra = "") {
 }
 
 var reqURL =
-	"https://api.rss2json.com/v1/api.json?rss_url=" +
+	"https://www.toptal.com/developers/feed2json/convert?url=" +
 	encodeURIComponent("https://symmetricalos.github.io/feeds/rss.xml");
 
 $.getJSON(reqURL, function (rss) {
 	var buffer = "";
-	//buffer = buffer + wrapHtml(rss.feed.title, "h1")
+	//buffer = buffer + wrapHtml(rss.title, "h1")
 	const items = rss.items;
 
 	console.log(rss);
@@ -19,9 +19,9 @@ $.getJSON(reqURL, function (rss) {
 		} else {
 			var title = wrapHtml(wrapHtml(item.title, "h2"), "a", " href=\"" + item.link + "\"");
 		}
-		var content = wrapHtml(item.content, "p");
+		var content = wrapHtml(item.content_html, "p");
 		var sub = wrapHtml(
-			"Published by " + item.author + " on " + item.pubDate,
+			"Published by " + item.author.name + " on " + item.date_published,
 			"small"
 		);
 		buffer = buffer + wrapHtml(title + content + sub, "div");
