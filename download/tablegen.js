810c5d1a2d9fe6ec1e9b248288a734dbad4e31d2
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-10-15 16:03:57 -0400

    sid stop breaking my code without telling me

--- download/tablegen.js
+++ download/tablegen.js
@@ -22,7 +22,7 @@ $.ajax({
       var version = wrapHtml(item.version, "th");
 			var size = wrapHtml(item.size, "th");
 			var sha = wrapHtml(item.sha1, "th");
-      var md5sum = wrapHtml(item.mdsum, "th");
+      var md5sum = wrapHtml(item.md5, "th");
 			buffer = buffer + wrapHtml(version + size + sha + md5sum, "tr");
 		}
 		document.getElementById("vertable").innerHTML = buffer;
