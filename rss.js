ffd70b0e4bb1e2900274b3fa14dd017d227c8d65
LeWolfYT <67524374+LeWolfYT@users.noreply.github.com>
2023-10-21 20:52:51 -0400

    fix escaped amper sanders

--- rss.js
+++ rss.js
@@ -9,7 +9,7 @@ $.ajax({
 	dataType: 'XML',
 	success: function (xml) {
 		var x2js = new X2JS();
-		var rss = x2js.xml2json(xml).rss.channel;
+		var rss = x2js.xml2json(xml.replace(/&(?!(?:apos|quot|[gl]t|amp);|#)/g, '&amp;')).rss.channel;
 		var buffer = "";
 		//buffer = buffer + wrapHtml(rss.feed.title, "h1")
 		const items = rss.item;
